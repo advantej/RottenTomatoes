d3d798e4891f01750c84c304d2fa593f9c7bf8df
Tejas Lagvankar <advantej@yahoo-inc.com>
2015-01-20 19:28:11 -0800

    Initial Commit

---
+++ Rotten Tomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten Tomatoes
+//
+//  Created by Tejas Lagvankar on 1/20/15.
+//  Copyright (c) 2015 Yahoo!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
